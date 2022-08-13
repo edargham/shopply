23a790b007cd49edbd233b3fbd90e8796ff4f407
Elias Dargham <edargham@hotmail.com>
2022-08-13 12:34:23 +0300

    13//08/2022 - Add Images to form

--- lib/screens/product_form_screen/product_form_screen.dart
+++ lib/screens/product_form_screen/product_form_screen.dart
@@ -15,12 +15,37 @@ class _ProductFormScreenState extends State<ProductFormScreen> {
   final FocusNode _priceFocusNode = FocusNode();
   final FocusNode _textFocusNode = FocusNode();
   final FocusNode _descriptionFocusNode = FocusNode();
+  final FocusNode _imageUrlFocusNode = FocusNode();
+  final TextEditingController _imageUrlTextBoxController =
+      TextEditingController();
+
+  @override
+  void initState() {
+    _imageUrlFocusNode.addListener(_onFocusChanged);
+  }
+
   @override
   void dispose() {
+    _imageUrlFocusNode.removeListener(_onFocusChanged);
     _priceFocusNode.dispose();
     _textFocusNode.dispose();
     _descriptionFocusNode.dispose();
+    _imageUrlFocusNode.dispose();
+    _imageUrlTextBoxController.dispose();
     super.dispose();
+  }
+
+  void _onFocusChanged() {
+    setState(() {});
+  }
+
+  Widget _showImage() {
+    return _imageUrlTextBoxController.text.isEmpty
+        ? const Align(
+            alignment: Alignment.center,
+            child: Text('Enter a URL'),
+          )
+        : FittedBox(child: Image.network(_imageUrlTextBoxController.text));
   }
 
   @override
@@ -70,9 +95,35 @@ class _ProductFormScreenState extends State<ProductFormScreen> {
                 maxLines: 3,
                 onChange: (_) {},
                 onSubmit: (_) {
-                  FocusScope.of(context).requestFocus(_textFocusNode);
+                  FocusScope.of(context).requestFocus(_imageUrlFocusNode);
                 },
                 caption: 'Description',
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Container(
+                  height: 256,
+                  margin: const EdgeInsets.fromLTRB(0.0, 4.0, 0.0, 16.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(4.0),
+                    border: Border.all(
+                      width: 2,
+                      color: Theme.of(context).colorScheme.background,
+                    ),
+                  ),
+                  child: _showImage(),
+                ),
+              ),
+              TextBox(
+                caption: 'Image URL',
+                actionButton: TextInputAction.next,
+                inputType: TextInputType.url,
+                focusNode: _imageUrlFocusNode,
+                controller: _imageUrlTextBoxController,
+                onChange: (_) {},
+                onSubmit: (_) {
+                  FocusScope.of(context).requestFocus(_textFocusNode);
+                },
               ),
             ],
           ),
