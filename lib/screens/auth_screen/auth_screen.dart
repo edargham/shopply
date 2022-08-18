711a16baea1f0b494d982f208fb3618d3c8f6450
Elias Dargham <edargham@hotmail.com>
2022-08-19 00:25:41 +0300

    19/08/2022 - Auth Screen Tabs

--- lib/screens/auth_screen/auth_screen.dart
+++ lib/screens/auth_screen/auth_screen.dart
@@ -8,6 +8,7 @@ class AuthScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        padding: const EdgeInsets.all(32.0),
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
         decoration: BoxDecoration(
@@ -16,9 +17,74 @@ class AuthScreen extends StatelessWidget {
               Theme.of(context).colorScheme.background.withOpacity(0.32),
               Theme.of(context).colorScheme.background,
             ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
           ),
         ),
-        child: const Center(child: Text('Auth Screen')),
+        child: Container(
+          padding: const EdgeInsets.all(8.0),
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.primary,
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+          child: Column(
+            children: <Widget>[
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 64.0),
+                child: Text(
+                  'Shopply',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 64.0,
+                  ),
+                ),
+              ),
+              const Center(
+                child: Text('Welcome to the greatest shop in the world.'),
+              ),
+              const SizedBox(height: 16.0),
+              DefaultTabController(
+                length: 2,
+                child: Expanded(
+                  child: Column(
+                    children: <Widget>[
+                      const TabBar(
+                        tabs: [
+                          Tab(
+                            text: 'Login',
+                            icon: Icon(Icons.login),
+                          ),
+                          Tab(
+                            text: 'Register',
+                            icon: Icon(Icons.person_add),
+                          ),
+                        ],
+                      ),
+                      Expanded(
+                        child: TabBarView(
+                          children: <Widget>[
+                            Container(
+                              child: const Center(
+                                child: Text('Login'),
+                              ),
+                            ),
+                            Container(
+                              child: const Center(
+                                child: Text('Register'),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
