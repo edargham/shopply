baad933a62660f55d831e4c4f35fc4c2ea4d753a
Elias Dargham <edargham@hotmail.com>
2023-04-11 22:58:19 +0300

    Elias Dargham 11/04/2022 - Login route.

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -3,11 +3,12 @@ import multer, { Multer, StorageEngine, FileFilterCallback } from 'multer';
 import { Model } from 'sequelize';
 import { v4 } from 'uuid';
 import { mkdirSync, existsSync, removeSync } from 'fs-extra';
-import { hash } from 'bcrypt';
+import { createHash } from 'crypto';
 
 
 import { physicalRootDir } from '../config/server.config';
 import { DestinationCallback, FileNameCallback, limits } from '../config/multer.config';
+import { authToken, generateAccessToken } from '../config/auth.config';
 
 import { UserModel, IUser } from '../models/user';
 
@@ -57,7 +58,7 @@ const uploads: Multer = multer({
  * @openapi
  * /api/users/signup:
  *  post:
- *    description: Signs up a new user for the application and stores their inf in the database.
+ *    description: Signs up a new user for the application and stores their information in the database.
  *    requestBody:
  *      required: true
  *      content:
@@ -103,11 +104,11 @@ const uploads: Multer = multer({
  *            password: Admin@1234
  *    responses:
  *      201:
- *        description: The product was successfuly created.
+ *        description: The user was successfuly created.
  *      400:
  *        description: The request failed validation.
  *      500:
- *        description: An internal error occured while creating the product in the database.
+ *        description: An internal error occured while creating the user in the database.
  *    tags:
  *      - Users
  */
@@ -118,7 +119,7 @@ router.post(
   async (req: Request, res: Response) => {
     try {
       const stamp: string = v4();
-      const password: string = await hash(`${ req.body.password }${ stamp }`, COST_SIZE);
+      const password: string =  createHash('sha512').update(`${ req.body.password }${ stamp }`).digest('hex');
       
       const rec: Model<IUser> = await UserModel.create({
         username: req.body.username,
@@ -156,14 +157,78 @@ router.post(
   }
 );
 
-
+/**
+ * @openapi
+ * /api/users/login:
+ *  post:
+ *    description: logs in a user and generates a token for their session.
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              username:
+ *                type: string
+ *              password:
+ *                type: string
+ *            required:
+ *              - username
+ *              - password
+ *          example:
+ *            username: tiesto
+ *            password: Admin@1234
+ *    responses:
+ *      200:
+ *        description: The authentication process completed.
+ *      500:
+ *        description: An internal error occured authenticating the user.
+ *    tags:
+ *      - Users
+ */
 router.post(
   '/login',
   async (req: Request, res: Response) => {
     try {
-      
-    } catch (error) {
+      const statusCode: number = StatusCodes.SUCCESS_CODE;
+      const authenticatingUser: UserModel | null = await UserModel.findOne({
+        where: {
+          username: req.body.username
+        }
+      });
 
+      if (authenticatingUser) {
+        const password: string = createHash('sha512').update(`${ req.body.password }${ authenticatingUser.get().stamp }`).digest('hex');
+        console.log(authToken);
+        console.log(authToken);
+        if (password == authenticatingUser.get().password) {
+          res.status(statusCode);
+          return res.json({
+              status: 200,
+              token: generateAccessToken(authenticatingUser.get(), authToken)
+          });
+        } else {
+          res.status(statusCode);
+          return res.json({
+            status: statusCode,
+            message: 'Incorrect username or password.'
+          });
+        }
+      } else {
+        res.status(statusCode);
+        return res.json({
+          status: statusCode,
+          message: 'Incorrect username or password.'
+        });
+      }
+    } catch (error) {
+      const statusCode: number = StatusCodes.INTERNAL_SERVER_ERROR;
+      return res.json({
+        status: statusCode,
+        message: 'Failed to authenticate user due to a server error. Please try again later.',
+        route: `POST ${ usersRouteName }/login`
+      });
     } 
   }
 );
