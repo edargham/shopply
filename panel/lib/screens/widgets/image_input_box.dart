a45bd0cb351838e80eac6b6e97a4f5a7dbdc74ff
Elias Dargham <edargham@hotmail.com>
2023-07-24 23:34:15 +0300

    Elias Dargham 07/24/2023 - Image Input.

---
+++ panel/lib/screens/widgets/image_input_box.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ImageInputBox extends StatefulWidget {
+  const ImageInputBox({Key? key}) : super(key: key);
+
+  @override
+  State<ImageInputBox> createState() => _ImageInputBoxState();
+}
+
+class _ImageInputBoxState extends State<ImageInputBox> {
+  @override
+  Widget build(BuildContext context) {}
+}
